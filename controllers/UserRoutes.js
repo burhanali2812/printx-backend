0bb5285e597bb15eb93e58ab3a7ead0aba488366
burhanali2812 <syedburhanali830@gmail.com>
2025-10-06 00:33:10 +0500

    add user3

--- controllers/UserRoutes.js
+++ controllers/UserRoutes.js
@@ -9,13 +9,13 @@ const authMiddleWare = require("../authMiddleWare");
 router.post("/register", async (req, res) => {
     const { name, email, password, role } = req.body;
     try {
-        // let user = await User.findOne({ email });
-        // if (user.length > 0) {
-        //     return res.status(400).json({ message: "User already exists" });
-        // }
+        let user = await User.findOne({ email });
+        if (user.length > 0) {
+            return res.status(400).json({ message: "User already exists" });
+        }
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
-       const user = new User({
+        user = new User({
             name, email, password: hashedPassword, role
         });
         await user.save();  

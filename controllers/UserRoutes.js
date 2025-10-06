283e07172a3f38bb99fa010418226ffd86c980a6
burhanali2812 <syedburhanali830@gmail.com>
2025-10-06 14:46:04 +0500

    login

--- controllers/UserRoutes.js
+++ controllers/UserRoutes.js
@@ -6,6 +6,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authMiddleWare = require("../authMiddleWare");
 
+
 router.post("/register", async (req, res) => {
     const { name, email, password, role } = req.body;
     try {
@@ -25,6 +26,31 @@ router.post("/register", async (req, res) => {
         res.status(500).send("Server error");
     }
     
+})
+router.post("/login", async (req, res) => {
+    const { email, password, role } = req.body;
+    try {
+        const user = await User.findOne({ email: email, role: role });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials Or Role" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+        const token = jwt.sign({ user: { id: user.id, role: user.role } }, process.env.JWT_SECRET, { expiresIn: "23h" });
+        const userData = {
+            id: user.id, 
+            name: user.name,
+             email: user.email, 
+             role: user.role
+        }
+        res.status(200).json({success : true, token: token , message: "Login Successful", userData });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+
 })
 
 

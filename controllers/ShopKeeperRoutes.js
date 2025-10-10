847e82a9fceef3d1b2a010ea45386f5ff5b70f7c
AshmalFatima <ashmalfatima25@gmail.com>
2025-10-10 02:59:35 -0700

    add shopkepper rutes

--- controllers/ShopKeeperRoutes.js
+++ controllers/ShopKeeperRoutes.js
@@ -5,4 +5,50 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authMiddleWare = require("../authMiddleWare");
 
+
+router.post("/register", async (req, res) => {
+    const { name, email, password, role } = req.body;
+    try {
+        let user = await Shopkeeper.findOne({ email: email });
+        if (user) {
+            return res.status(400).json({success : false, message: "Shopkeeper already exists" });
+        }
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+        user = new Shopkeeper({
+            name, email, password: hashedPassword, role
+        });
+        await user.save();  
+        res.status(201).json({success : true,  message: "Shopkeeper registered successfully" });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+    
+})
+router.post("/login", async (req, res) => {
+    const { email, password, role } = req.body;
+    try {
+        const user = await Shopkeeper.findOne({ email: email, role: role });
+        if (!user) {
+            return res.status(400).json({ message: "Invalid Credentials Or Role" });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ message: "Invalid Credentials" });
+        }
+        const token = jwt.sign({ user: { id: user.id, role: user.role } }, process.env.JWT_SECRET, { expiresIn: "23h" });
+        const userData = {
+            id: user.id, 
+            name: user.name,
+             email: user.email, 
+             role: user.role
+        }
+        res.status(200).json({success : true, token: token , message: "Login Successful", userData });
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send("Server error");
+    }
+
+})
 module.exports = router;
